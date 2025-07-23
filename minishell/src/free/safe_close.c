12cd3d1202c9658d1c2a698781185fb7260098a5
Xasiyy <xasidid2441@gmail.com>
2025-07-23 21:56:41 +0200

    fix(safe_close); supression of fcntl in safe_close

--- minishell/src/free/safe_close.c
+++ minishell/src/free/safe_close.c
@@ -6,7 +6,7 @@
 /*   By: asdiallo <asiya040906@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 17:37:47 by asdiallo          #+#    #+#             */
-/*   Updated: 2025/06/28 17:39:49 by asdiallo         ###   ########.fr       */
+/*   Updated: 2025/07/23 21:54:15 by asdiallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ int	safe_close(int *fd)
 {
 	if (!fd || *fd < 0)
 		return (0);
-	if (fcntl(*fd, F_GETFD) == -1 && errno == EBADF)
-	{
-		*fd = -1;
-		return (0);
-	}
 	if (close(*fd) == -1)
+	{
+		if (errno == EBADF)
+		{
+			*fd = -1;
+			return (0);
+		}
 		return (-1);
+	}
 	*fd = -1;
 	return (0);
 }
